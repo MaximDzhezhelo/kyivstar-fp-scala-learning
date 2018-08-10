150ccc6968067f74c2ea506c5edb5eb2df442d64
MaximDzhezhelo <mdzhezhelo@gmail.com>
2018-08-10 19:37:11 +0300

    init value;

--- src/main/scala/l02/Example.scala
+++ src/main/scala/l02/Example.scala
@@ -4,9 +4,11 @@ object Example {
 
   def main(args: Array[String]): Unit = {
     val x = 2 + 2              // final value, init in start, can't override;
+    val q: java.lang.String = "Hello wild world" // explicit type indication;
+    val b: String = "Hello wild world"
     var y = 2 * 2              // variable value, init in start, can override;
-    def z  = 2 + 3         //  definition value, init in execution
-    lazy val a = 2 * 2          // final value, init in execution
+    def z  = 2 + 3         //  definition value, init in execution;
+    lazy val a = 2 * 2          // final value, init in execution;
 
     println(z)
     println(a)
